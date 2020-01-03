5bfff9cc976fe8a24ffb93aa9cffe592d21a504f
oren <virtualoren@gmail.com>
2020-01-02 22:36:56 -0600

    Added instructions to install script to copy server.py and config.json

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 cp horizontal_scaler.service /etc/systemd/system/
 cp load-balancer.conf /etc/nginx/conf.d/
+mkdir -p /usr/bin/horizontal_scaler
+cp server.py /usr/bin/horizontal_scaler/
+cp config.json /usr/bin/horizontal_scaler/
